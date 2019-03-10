71a1b5cbd2b01aa407cc02e600b548e6ad4f9157
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-03-10 01:23:43 -0500

    sql connect

---
+++ server/src/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').load()
+
+module.exports = {
+  port: 8081,
+  db: {
+    database: process.env.MYSQL_DB,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+
+    insecureAuth: true,
+    options: {
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3307
+    }
+  }
+}
