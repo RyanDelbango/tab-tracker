d1f523fd86872303b0f17888045df16c19245ccd
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-03-10 16:13:54 -0400

    Joi and vuetify setup

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-  <h1>Register</h1>
+  <v-layout row wrap>
+    <v-flex xs10 offset-xs1>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+  <div class="pl-4 pr-4 pt-2 pb-2">
   <input
     type="email"
     name="email"
@@ -13,11 +18,19 @@
     v-model="password"
     placeholder="password"/>
     <br>
-    <button
+    <div class="error" v-html="error" />
+    <br>
+    <v-btn
+      class="cyan"
       @click="register">
       Register
-      </button>
-  </div>
+    </v-btn>
+      </div>
+
+      </div>
+    </v-flex>
+  </v-layout>
+
 </template>
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
@@ -25,19 +38,26 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 <style scoped>
+.error {
+  color: red;
+}
 </style>
